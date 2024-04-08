42747f7987cf288cc8554bf55172fd61a683a7a4
BerserkZak <me4anicman@gmail.com>
2024-04-09 00:35:36 +0300

    Nothing added

--- src/main/java/dev/aztecsolutions/inventorylevel/listeners/InventoryOpenListener.java
+++ src/main/java/dev/aztecsolutions/inventorylevel/listeners/InventoryOpenListener.java
@@ -1,12 +1,25 @@
 package dev.aztecsolutions.inventorylevel.listeners;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.jetbrains.annotations.NotNull;
 
 public class InventoryOpenListener implements Listener {
     @EventHandler
     public void onPlayerOpeningEnderchest(InventoryOpenEvent e){
+
+        @NotNull InventoryType playerInventory = e.getPlayer().getInventory().getType();
+        Player player = (Player) e.getPlayer();
+
+        if (!playerInventory.equals(InventoryType.ENDER_CHEST)) {
+            if (player.hasPermission("aztecsolutions.levels.1")) {
+            }
+
+    }
+
 
     }
 }
