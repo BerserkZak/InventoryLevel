363e99b2faff65eff18ce3974092937092d8fe3a
BerserkZak <me4anicman@gmail.com>
2024-04-09 00:02:33 +0300

    Preparing, nothing added

---
+++ src/main/java/dev/aztecsolutions/inventorylevel/commands/EndInvCommand.java
@@ -0,0 +1,13 @@
+package dev.aztecsolutions.inventorylevel.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class EndInvCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        return false;
+    }
+}
--- src/main/java/dev/aztecsolutions/inventorylevel/core/InventoryLevel.java
+++ src/main/java/dev/aztecsolutions/inventorylevel/core/InventoryLevel.java
@@ -1,12 +1,13 @@
-package dev.aztecsolutions.inventorylevel;
+package dev.aztecsolutions.inventorylevel.core;
 
+import dev.aztecsolutions.inventorylevel.commands.EndInvCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class InventoryLevel extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getCommand("endinv").setExecutor(new EndInvCommand());
 
     }
 
