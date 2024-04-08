3726ff8c1fdb7eb0a0e1b542839a47781afa0840
BerserkZak <me4anicman@gmail.com>
2024-04-08 23:18:29 +0300

    Initial commit

---
+++ src/main/java/dev/aztecsolutions/inventorylevel/InventoryLevel.java
@@ -0,0 +1,17 @@
+package dev.aztecsolutions.inventorylevel;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class InventoryLevel extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
